3f797f39d3c8d63232760731258982e36271e9b6
GuoXin <githublol@guoxin.win>
2025-01-06 23:33:17 +0800

    -

---
+++ src/routes/(app)/workspace/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { user } from '$lib/stores';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		if ($user?.role !== 'admin') {
+			if ($user?.permissions?.workspace?.models) {
+				goto('/workspace/models');
+			} else {
+				goto('/');
+			}
+		} else {
+			goto('/workspace/models');
+		}
+	});
+</script>
